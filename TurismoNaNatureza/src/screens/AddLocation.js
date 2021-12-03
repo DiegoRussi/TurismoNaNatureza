293d12b88d799bc58331f3b569cf78cc7cf14323
Diego Russi <digomatheusrussi@gmail.com>
2021-12-03 17:07:26 -0300

    Added Flatlist to Location screens form

--- TurismoNaNatureza/src/screens/AddLocation.js
+++ TurismoNaNatureza/src/screens/AddLocation.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
-import { View, Text, Image, Button, SafeAreaView, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  View,
+  FlatList,
+  Text,
+  Image,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  Linking,
+  StyleSheet
+} from 'react-native';
 
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -11,14 +21,14 @@ import ViewPager from '@react-native-community/viewpager';
 import styles from '../styles/locationStyles'
 import starStyles from '../styles/starStyles'
 
-// import getLocationInfo from '../functions/database.js'
+// import getLocationInfo from '../database/LocationHelper.js'
 
-const Location = ({ is_add, location_id, lat, long, ...inputProps }) => {
-  console.log("Location DEBUG");
-  console.log("location_id = ", location_id);
-
-  const latitude = lat;
+const AddLocation = ({ long, lat}) => {
+  console.log("AddLocation DEBUG");
   const longitude = long;
+  const latitude = lat;
+  console.log("longitude = ", longitude);
+  console.log("latitude = ", latitude);
   const type = "";
   const title = "";
   const desc = "";
@@ -35,8 +45,8 @@ const Location = ({ is_add, location_id, lat, long, ...inputProps }) => {
   //   ] = getLocationInfo(location_id);
   // }
 
-  const [currentLatitude, setCurrentLatitude] = useState(latitude);
   const [currentLongitude, setCurrentLongitude] = useState(longitude);
+  const [currentLatitude, setCurrentLatitude] = useState(latitude);
   const [locationType, setLocationType] = useState(type);
   const [locationTitle, setLocationTitle] = useState(title);
   const [locationDesc, setLocationDesc] = useState(desc);
@@ -79,101 +89,159 @@ const Location = ({ is_add, location_id, lat, long, ...inputProps }) => {
     console.log("starRate = ", starRate);
   }
 
-  return (
-    <View style={styles.container}>
+  const shareLocation = () => {
+    console.log("TO BE IMPLEMENTED")
+  }
 
-      <View style={styles.subContainer}>
-        <Image style={styles.logo} source={{uri: 'https://www.iconsdb.com/icons/preview/green/map-marker-xxl.png'}} />
-        <Text style={styles.title}>Coordenadas</Text>
-        <Text style={styles.subtitle}>Latitude: {currentLatitude}</Text>
-        <Text style={styles.subtitle}>Longitude: {currentLongitude}</Text>
-      </View>
+  const starLocation = () => {
+    console.log("TO BE IMPLEMENTED")
+  }
 
-      <View style={styles.form}>
-        <Picker
-          prompt={'Tipo de Local'}
-          selectedValue={locationType}
-          onValueChange={(itemValue, itemIndex) =>
-            setLocationType(itemValue)
-          } 
-          >
-          <Picker.Item label="Selecione o Tipo de Local" value="0" />
-          <Picker.Item label="Paisagem" value="paisagem" />
-          <Picker.Item label="Fauna" value="fauna" />
-          <Picker.Item label="Flora" value="flora" />
-          <Picker.Item label="Cachoeiras" value="cachoeiras" />
-          <Picker.Item label="Riachos" value="riachos" />
-          <Picker.Item label="Montanhas" value="montanhas" />
-          <Picker.Item label="Referência para Trilha" value="trilhas" />
-        </Picker>
+  const openLocationRoute = () => {
+    console.log("openLocationRoute")
+    Linking.openURL(
+      // `http://www.google.com/maps/place/${currentLatitude},${currentLongitude}`
+      `https://maps.google.com/?q=${currentLatitude},${currentLongitude}`
+    );
+  }
 
-        <TextInput style={styles.text}
-          mode="outlined"
-          label="Título"
-          placeholder="Insira um Título"
-          value={locationTitle}
-          onChangeText={locationTitle => setLocationTitle(locationTitle)}
-        />
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    }
+  ];
 
-        <TextInput style={styles.text}
-          mode="outlined"
-          label="Descrição"
-          placeholder="Insira uma Descrição"
-          value={locationDesc}
-          onChangeText={locationDesc => setLocationDesc(locationDesc)}
-          multiline={true}
-          numberOfLines={4}
-        />
-
-        <Text style={starStyles.textStyle}>Imagens</Text>
-        <ViewPager
-          pageMargin={1}
-          style={{ height: 160 }}>
-          <View>
-            <Image source={{uri: locationImages[0]}} style={{width: 420, height: 170}}/>
-          </View>
-          <View>
-            <Image source={{uri: locationImages[1]}} style={{width: 420, height: 170}}/>
-          </View>
-          <View>
-            <Image source={{uri: locationImages[2]}} style={{width: 420, height: 170}}/>
-          </View>
-        </ViewPager>
-
-      </View>
-
-      <SafeAreaView style={starStyles.container}> 
-        <View style={starStyles.container}> 
-          <Text style={starStyles.textStyle}> 
-            Avaliação: {starRate} / {Math.max.apply(null, starRatings)} 
-          </Text>
-          {RatingStars()}
-        </View> 
-      </SafeAreaView> 
-
-      <View style={styles.button}>
-          <Button
-            title='Salvar'
-            onPress={() => newLocation()}
-          />
-      </View>
-
-      <ActionButton buttonColor="rgba(1, 152, 117, 1)">
-          <ActionButton.Item buttonColor='#9b59b6' title="Compartilhar" onPress={() => shareLocation()}>
-            <Icon name="md-share" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Avaliar" onPress={() => starLocation()}>
-            <Icon name="md-star" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Rota" onPress={() => openRouteLocation()}>
-            <Icon name="md-map-outline" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Voltar" onPress={() => this.props.navigation.navigate('Map')}>
-            <Icon name="md-return-up-back-sharp" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-        </ActionButton>
-
+  const Header = () => (
+    <View style={styles.subContainer}>
+      <Image style={styles.logo} source={{uri: 'https://www.iconsdb.com/icons/preview/green/map-marker-xxl.png'}} />
+      <Text style={styles.title}>Coordenadas</Text>
+      <Text style={styles.subtitle}>Latitude: {currentLatitude}</Text>
+      <Text style={styles.subtitle}>Longitude: {currentLongitude}</Text>
     </View>
+  );
+
+  const Type = () => (
+    <Picker
+      prompt={'Tipo de Local'}
+      selectedValue={locationType}
+      onValueChange={(itemValue, itemIndex) =>
+        setLocationType(itemValue)
+      }
+      >
+      <Picker.Item label="Selecione o Tipo de Local" value="0" />
+      <Picker.Item label="Paisagem" value="paisagem" />
+      <Picker.Item label="Fauna" value="fauna" />
+      <Picker.Item label="Flora" value="flora" />
+      <Picker.Item label="Cachoeiras" value="cachoeiras" />
+      <Picker.Item label="Riachos" value="riachos" />
+      <Picker.Item label="Montanhas" value="montanhas" />
+      <Picker.Item label="Referência para Trilha" value="trilhas" />
+    </Picker>
+  );
+
+  const Title = () => (
+    <View style={styles.form}>
+      <TextInput style={styles.text}
+        mode="outlined"
+        label="Título"
+        placeholder="Insira um Título"
+        value={locationTitle}
+        onChangeText={locationTitle => setLocationTitle(locationTitle)}
+      />
+    </View>
+  );
+
+  const Description = () => (
+    <TextInput style={styles.text}
+      mode="outlined"
+      label="Descrição"
+      placeholder="Insira uma Descrição"
+      value={locationDesc}
+      onChangeText={locationDesc => setLocationDesc(locationDesc)}
+      multiline={true}
+      numberOfLines={4}
+    />
+  );
+
+  const Images = () => (
+    <View>
+      <Text style={starStyles.textStyle}>Imagens</Text>
+      <ViewPager
+        pageMargin={1}
+        style={{ height: 250 }}>
+        <View>
+          <Image source={{uri: locationImages[0]}} style={{width: 420, height: 250}}/>
+        </View>
+        <View>
+          <Image source={{uri: locationImages[1]}} style={{width: 420, height: 250}}/>
+        </View>
+        <View>
+          <Image source={{uri: locationImages[2]}} style={{width: 420, height: 250}}/>
+        </View>
+      </ViewPager>
+    </View>
+  );
+
+  const Stars = () => (
+    <SafeAreaView style={starStyles.container}>
+      <View style={starStyles.container}>
+        <Text style={starStyles.textStyle}>
+          Avaliação: {starRate} / {Math.max.apply(null, starRatings)}
+        </Text>
+        {RatingStars()}
+      </View>
+    </SafeAreaView>
+  );
+
+
+  const SaveButton = () => (
+    <View style={styles.button}>
+      <Button
+        title='Salvar'
+        onPress={() => newLocation()}
+      />
+    </View>
+  );
+
+  const renderItem = ({ item }) => (
+    <View>
+      <View style={styles.subContainer}>
+        <Header />
+      </View>
+      <View style={styles.form}>
+        <Type />
+        <Title />
+        <Description />
+        <Images />
+        <Stars />
+        <SaveButton />
+      </View>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      <ActionButton buttonColor="rgba(1, 152, 117, 1)">
+        <ActionButton.Item buttonColor='#9b59b6' title="Compartilhar" onPress={() => shareLocation()}>
+          <Icon name="md-share" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor='#3498db' title="Avaliar" onPress={() => starLocation()}>
+          <Icon name="md-star" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor='#3498db' title="Como Chegar" onPress={() => openLocationRoute()}>
+          <Icon name="md-map-outline" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor='#3498db' title="Voltar" onPress={() => this.props.navigation.navigate('Map')}>
+          <Icon name="md-return-up-back-sharp" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+      </ActionButton>
+    </SafeAreaView>
   );
 }
 
@@ -185,16 +253,4 @@ const stylesFAB = StyleSheet.create({
   },
 });
 
-function shareLocation(){
-  console.log("TO BE IMPLEMENTED")
-}
-
-function starLocation(){
-  console.log("TO BE IMPLEMENTED")
-}
-
-function openRouteLocation(){
-  console.log("TO BE IMPLEMENTED")
-}
-
-export default Location;
+export default AddLocation;
--- TurismoNaNatureza/src/screens/location.js
+++ TurismoNaNatureza/src/screens/location.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {
   View,
+  FlatList,
   Text,
   Image,
   Button,
@@ -20,7 +21,7 @@ import ViewPager from '@react-native-community/viewpager';
 import styles from '../styles/locationStyles'
 import starStyles from '../styles/starStyles'
 
-// import getLocationInfo from '../functions/LocationHelper.js'
+// import getLocationInfo from '../database/LocationHelper.js'
 
 const Location = ({ location_id, l_title, location}) => {
   console.log("Location DEBUG");
@@ -92,14 +93,14 @@ const Location = ({ location_id, l_title, location}) => {
     console.log("starRate = ", starRate);
   }
 
-  function shareLocation(){
+  const shareLocation = () => {
     console.log("TO BE IMPLEMENTED")
   }
-  
-  function starLocation(){
+
+  const starLocation = () => {
     console.log("TO BE IMPLEMENTED")
   }
-  
+
   const openLocationRoute = () => {
     console.log("openLocationRoute")
     Linking.openURL(
@@ -108,101 +109,144 @@ const Location = ({ location_id, l_title, location}) => {
     );
   }
 
-  return (
-    <View style={styles.container}>
+  
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    }
+  ];
 
-      <View style={styles.subContainer}>
-        <Image style={styles.logo} source={{uri: 'https://www.iconsdb.com/icons/preview/green/map-marker-xxl.png'}} />
-        <Text style={styles.title}>Coordenadas</Text>
-        <Text style={styles.subtitle}>Latitude: {currentLatitude}</Text>
-        <Text style={styles.subtitle}>Longitude: {currentLongitude}</Text>
-      </View>
-
-      <View style={styles.form}>
-        <Picker
-          prompt={'Tipo de Local'}
-          selectedValue={locationType}
-          onValueChange={(itemValue, itemIndex) =>
-            setLocationType(itemValue)
-          } 
-          >
-          <Picker.Item label="Selecione o Tipo de Local" value="0" />
-          <Picker.Item label="Paisagem" value="paisagem" />
-          <Picker.Item label="Fauna" value="fauna" />
-          <Picker.Item label="Flora" value="flora" />
-          <Picker.Item label="Cachoeiras" value="cachoeiras" />
-          <Picker.Item label="Riachos" value="riachos" />
-          <Picker.Item label="Montanhas" value="montanhas" />
-          <Picker.Item label="Referência para Trilha" value="trilhas" />
-        </Picker>
-
-        <TextInput style={styles.text}
-          mode="outlined"
-          label="Título"
-          placeholder="Insira um Título"
-          value={locationTitle}
-          onChangeText={locationTitle => setLocationTitle(locationTitle)}
-        />
-
-        <TextInput style={styles.text}
-          mode="outlined"
-          label="Descrição"
-          placeholder="Insira uma Descrição"
-          value={locationDesc}
-          onChangeText={locationDesc => setLocationDesc(locationDesc)}
-          multiline={true}
-          numberOfLines={4}
-        />
-
-        <Text style={starStyles.textStyle}>Imagens</Text>
-        <ViewPager
-          pageMargin={1}
-          style={{ height: 160 }}>
-          <View>
-            <Image source={{uri: locationImages[0]}} style={{width: 420, height: 170}}/>
-          </View>
-          <View>
-            <Image source={{uri: locationImages[1]}} style={{width: 420, height: 170}}/>
-          </View>
-          <View>
-            <Image source={{uri: locationImages[2]}} style={{width: 420, height: 170}}/>
-          </View>
-        </ViewPager>
-
-      </View>
-
-      <SafeAreaView style={starStyles.container}> 
-        <View style={starStyles.container}> 
-          <Text style={starStyles.textStyle}> 
-            Avaliação: {starRate} / {Math.max.apply(null, starRatings)} 
-          </Text>
-          {RatingStars()}
-        </View> 
-      </SafeAreaView> 
-
-      <View style={styles.button}>
-          <Button
-            title='Salvar'
-            onPress={() => newLocation()}
-          />
-      </View>
-
-      <ActionButton buttonColor="rgba(1, 152, 117, 1)">
-          <ActionButton.Item buttonColor='#9b59b6' title="Compartilhar" onPress={() => shareLocation()}>
-            <Icon name="md-share" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Avaliar" onPress={() => starLocation()}>
-            <Icon name="md-star" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Como Chegar" onPress={() => openLocationRoute()}>
-            <Icon name="md-map-outline" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Voltar" onPress={() => this.props.navigation.navigate('Map')}>
-            <Icon name="md-return-up-back-sharp" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-        </ActionButton>
-
+  const Header = () => (
+    <View style={styles.subContainer}>
+      <Image style={styles.logo} source={{uri: 'https://www.iconsdb.com/icons/preview/green/map-marker-xxl.png'}} />
+      <Text style={styles.title}>Coordenadas</Text>
+      <Text style={styles.subtitle}>Latitude: {currentLatitude}</Text>
+      <Text style={styles.subtitle}>Longitude: {currentLongitude}</Text>
     </View>
+  );
+
+  const Type = () => (
+    <Picker
+      prompt={'Tipo de Local'}
+      selectedValue={locationType}
+      onValueChange={(itemValue, itemIndex) =>
+        setLocationType(itemValue)
+      } 
+      >
+      <Picker.Item label="Selecione o Tipo de Local" value="0" />
+      <Picker.Item label="Paisagem" value="paisagem" />
+      <Picker.Item label="Fauna" value="fauna" />
+      <Picker.Item label="Flora" value="flora" />
+      <Picker.Item label="Cachoeiras" value="cachoeiras" />
+      <Picker.Item label="Riachos" value="riachos" />
+      <Picker.Item label="Montanhas" value="montanhas" />
+      <Picker.Item label="Referência para Trilha" value="trilhas" />
+    </Picker>
+  );
+
+  const Title = () => (
+    <View style={styles.form}>
+      <TextInput style={styles.text}
+        mode="outlined"
+        label="Título"
+        placeholder="Insira um Título"
+        value={locationTitle}
+        onChangeText={locationTitle => setLocationTitle(locationTitle)}
+      />
+    </View>
+  );
+
+  const Description = () => (
+    <TextInput style={styles.text}
+      mode="outlined"
+      label="Descrição"
+      placeholder="Insira uma Descrição"
+      value={locationDesc}
+      onChangeText={locationDesc => setLocationDesc(locationDesc)}
+      multiline={true}
+      numberOfLines={4}
+    />
+  );
+
+  const Images = () => (
+    <View>
+      <Text style={starStyles.textStyle}>Imagens</Text>
+      <ViewPager
+        pageMargin={1}
+        style={{ height: 250 }}>
+        <View>
+          <Image source={{uri: locationImages[0]}} style={{width: 420, height: 250}}/>
+        </View>
+        <View>
+          <Image source={{uri: locationImages[1]}} style={{width: 420, height: 250}}/>
+        </View>
+        <View>
+          <Image source={{uri: locationImages[2]}} style={{width: 420, height: 250}}/>
+        </View>
+      </ViewPager>
+    </View>
+  );
+
+  const Stars = () => (
+    <SafeAreaView style={starStyles.container}> 
+      <View style={starStyles.container}>
+        <Text style={starStyles.textStyle}>
+          Avaliação: {starRate} / {Math.max.apply(null, starRatings)}
+        </Text>
+        {RatingStars()}
+      </View>
+    </SafeAreaView>
+  );
+
+
+  const SaveButton = () => (
+    <View style={styles.button}>
+      <Button
+        title='Salvar'
+        onPress={() => newLocation()}
+      />
+    </View>
+  );
+
+  const renderItem = ({ item }) => (
+    <View>
+      <View style={styles.subContainer}>
+        <Header />
+      </View>
+      <View style={styles.form}>
+        <Type />
+        <Title />
+        <Description />
+        <Images />
+        <Stars />
+        <SaveButton />
+      </View>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      <ActionButton buttonColor="rgba(1, 152, 117, 1)">
+        <ActionButton.Item buttonColor='#9b59b6' title="Compartilhar" onPress={() => shareLocation()}>
+          <Icon name="md-share" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor='#3498db' title="Avaliar" onPress={() => starLocation()}>
+          <Icon name="md-star" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor='#3498db' title="Como Chegar" onPress={() => openLocationRoute()}>
+          <Icon name="md-map-outline" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor='#3498db' title="Voltar" onPress={() => this.props.navigation.navigate('Map')}>
+          <Icon name="md-return-up-back-sharp" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+      </ActionButton>
+    </SafeAreaView>
   );
 }
 
